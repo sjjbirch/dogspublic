368c56e9a07d4bc599b038064db3b7583c40217a
sjjbirch <birch.jj@gmail.com>
2022-03-20 14:45:51 +1000

    reset date time format initialiser to default from australian format because it made heroku die the other way. God bless america.

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -2,10 +2,12 @@
 
 # amazingly all three of these are needed despite seeming redundant
 
-Date::DATE_FORMATS[:default] = '%d/%m/%Y'
+# Date::DATE_FORMATS[:default] = '%d/%m/%Y'
 
-# if you want to change the format of Time display then add the line below
-Time::DATE_FORMATS[:default]= '%d/%m/%Y %H:%M:%S'
+# # if you want to change the format of Time display then add the line below
+# Time::DATE_FORMATS[:default]= '%d/%m/%Y %H:%M:%S'
 
-# if you want to change the DB date format.
-Time::DATE_FORMATS[:db]= '%d/%m/%Y %H:%M:%S'
+# # if you want to change the DB date format.
+# Time::DATE_FORMATS[:db]= '%d/%m/%Y %H:%M:%S'
+
+# all of this had to be nuked because heroku is awful
