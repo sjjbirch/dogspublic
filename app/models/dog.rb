93b526e3f33e169a5724a4e97994178ca4927d63
sjjbirch <birch.jj@gmail.com>
2022-03-20 15:02:13 +1000

    Fixed validation error that occurred only on heroku.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,7 @@ class Dog < ApplicationRecord
     has_one :advert, dependent: :destroy
 
     validates :rname, length: { in: 5..100 }, uniqueness: true
-    validates :cname, length: { in: 5..100 }
+    validates :cname, length: { in: 1..100 }
     validates :handler, length: { in: 5..100 }
     validates :sex, length: { is: 1 }
     validates :dob, presence: true
