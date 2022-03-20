ae90078a706936f96467ce18fc92998647556443
sjjbirch <birch.jj@gmail.com>
2022-03-20 14:20:13 +1000

    assets configured for deployment

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Dogs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.serve_static_assets = true
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +19,6 @@ module Dogs
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
   end
 end
